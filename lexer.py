62b66d23c9955000e9e969f9e13cc7b556c6ff23
Khoi <anhkhoijean@gmail.com>
2025-11-15 10:49:07 +0100

    Optimize the first part, need to test more, left the old code in comments in case of we need to reuse

--- lexer.py
+++ lexer.py
@@ -182,153 +182,265 @@ def read_INT():
 global int_value
 global exp_value
 global sign_value
+global float_value
+global apres_virgule
+global apres_exp
+
 
 def automate_num(etat, input):
-    flottant_sym = ['e', 'E']
+    # print(input)
     match etat:
         case 0:
             if input in defs.DIGITS:
                 etat = 3
-            elif next == '.':
+            elif input == '.':
                 etat = 1
             else:
                 etat = 7
         case 1:
-            if next in defs.DIGITS:
+            if input in defs.DIGITS:
                 etat = 2
             else:
                 etat = 7
         case 2:
-            if next in defs.DIGITS:
+            # print(f"input = {input}")
+            if input in defs.DIGITS:
+                # print(f"input = {input}")
                 etat = 2
-            elif next in flottant_sym:
+            elif input in ('e', 'E'):
                 etat = 4
             else:
                 etat = 7
         case 3:
-            if next in defs.DIGITS:
+            if input in defs.DIGITS:
                 etat = 3
-            elif next in flottant_sym:
+            elif input in ('e', 'E'):
                 etat = 4
-            elif next == '.':
+            elif input == '.':
                 etat = 2
             else: 
                 etat = 7
         case 4:
-            if next in defs.DIGITS:
+            if input in defs.DIGITS:
                 etat = 6
-            elif next in ['+','-']:
+            elif input in ['+','-']:
                 etat = 5
             else:
                 etat = 7
         case 5:
-            if next in defs.DIGITS:
+            if input in defs.DIGITS:
                 etat = 6
             else:
                 etat = 7
         case 6:
-            if next in defs.DIGITS:
+            if input in defs.DIGITS:
                 etat = 6
             else:
                 etat = 7
-    print(f"etat = {etat}")
+    # print(f"etat = {etat}")
     return etat
 
+def automate_sim(automate, etat, char):
+    if char in defs.DIGITS:
+        entree = 1
+    elif char == '.':
+        entree = 2
+    elif char in ('E', 'e'):
+        entree = 3
+    elif char in ('+', '-'):
+        entree = 4
+    else: 
+        entree = 5
+    return automate[entree - 1][etat]
+
+def char_to_val(caractere):
+    global int_value
+    global exp_value
+    global sign_value
+    global float_value
+    global apres_virgule
+    global apres_exp
+
+    if caractere == '.':
+        apres_virgule = 1
+        consume_char()
+    elif caractere in ('e', 'E'):
+        apres_exp = 1
+        consume_char()
+    elif caractere in ('+','-'):
+        if caractere == '-':
+            sign_value = -1
+        consume_char()
+    elif caractere in defs.DIGITS:
+        if apres_exp:
+            exp_value = exp_value*10 + read_digit()
+        elif apres_virgule:
+            float_value -= 1
+            int_value = int_value*10 + read_digit()
+        else:
+            int_value = int_value*10 + read_digit()
 
 
 # Lecture d'un nombre en renvoyant sa valeur
 def read_NUM():
+    global int_value
+    global exp_value
+    global sign_value
+    global float_value
+    global apres_virgule
+    global apres_exp
+
+    automate = [[3,2,2,3,6,6,6,7],
+                [1,7,7,2,7,7,7,7],
+                [7,7,4,4,7,7,7,7],
+                [7,7,7,7,5,7,7,7],
+                [7,7,7,7,7,7,7,7]]
+
     etat = 0
-    next = peek_char1()
-    mantisse = 0
-    flottant_sym = ['e', 'E']
-    i_flott = -1
-    pow = 0
-    sign = 1
+    etat_3 = 0
+    char_3 = peek_char3()
+    char_1 = peek_char1()
+    # 0 => not stuck
+    stuck = 0
+    lexeme = ""
+    int_value = 0
+    exp_value = 0
+    sign_value = 1
+    float_value = 0
+    apres_virgule = 0
+    apres_exp = 0
+
+    if automate_num(etat, char_1) == 7:
+        return None
+
     while True:
-        match etat:
-            case 0:
-                if next in defs.DIGITS:
-                    etat = 3
-                    mantisse = mantisse*10 + read_digit()
-                elif next == '.':
-                    etat = 1
-                    consume_char()
-                else:
-                    return None
-            case 1:
-                if next in defs.DIGITS:
-                    etat = 2
-                    mantisse = mantisse + read_digit()*10**i_flott
-                    i_flott -= 1
-                else:
-                    return None
-            case 2:
-                if next in defs.DIGITS:
-                    etat = 2
-                    mantisse = mantisse + read_digit()*10**i_flott
-                    i_flott -= 1
-                elif next in flottant_sym:
-                    etat = 4
-                    consume_char()
-                else:
-                    return mantisse
-            case 3:
-                if next in defs.DIGITS:
-                    etat = 3
-                    mantisse = mantisse * 10 + read_digit()
-                elif next in flottant_sym:
-                    etat = 4
-                    consume_char()
-                elif next == '.':
-                    etat = 2
-                    consume_char()
-                else: 
-                    return mantisse
-            case 4:
-                if next in defs.DIGITS:
-                    etat = 6
-                    pow = pow * 10 + read_digit()
-                elif next in ['+','-']:
-                    etat = 5
-                    if next == '-':
-                        sign = -1
-                    consume_char()
-                else:
-                    return mantisse
-            case 5:
-                if next in defs.DIGITS:
-                    etat = 6
-                    pow = pow*10 + read_digit()
-                else:
-                    return mantisse
-            case 6:
-                if next in defs.DIGITS:
-                    etat = 6
-                    pow = pow*10 + read_digit()
-                else:
-                    return mantisse*10**(pow*sign)
-        next = peek_char1()
-    # return mantisse*10**(pow*sign);
-    return None
+        etat_3 = etat
+        for i in char_3:
+            etat_3 = automate_sim(automate, etat_3, i) 
+            # etat_3 = automate_num(etat_3, i)
+            # print(etat_test == etat_3)
+            # print(etat_3)
+            if etat_3 == 7:
+                stuck = 1
+                break
+
+        if stuck:
+            break
+        else:
+            # consume_char()
+            etat = automate_sim(automate, etat, char_1)
+            # lexeme += char_1
+            char_to_val(char_1)
+            char_1 = peek_char1()
+            char_3 = peek_char3()
+    
+
+    # On verifie tout les 2 charactere s'ils nous ramenent a Rome (etat final)
+    # si c'est un etat final donc on accepte ce mot, on sait bien que le dernier charactere ca nous donne un etat puit
+    # On ne consume pas la caractère si on sait pas on est bien dans l'etat final
+    if automate_sim(automate,automate_sim(automate, etat, char_3[0]),char_3[1]) in (2,3,6):
+        # lexeme += char_3[0]
+        # lexeme += char_3[1]
+        char_to_val(char_3[0])
+        char_to_val(char_3[1])
+        # c'est juste pour dire que on est bien dans un etat final, on peut mettre 2 ou 3 ou 6
+        etat = 2
+        # consume_char()
+        # consume_char()
+    else:
+        if automate_sim(automate, etat, char_3[0]) in (2,3,6):
+            char_to_val(char_3[0])
+            # c'est juste pour dire que on est bien dans un etat final, on peut mettre 2 ou 3 ou 6
+            etat = 2
+            # lexeme += char_3[0]
+            # consume_char()
+    # if lexeme == "":
+    #     return None
+    # print(f"int_value = {int_value}, exp_value = {exp_value}, sign_value = {sign_value}, float_value = {float_value}")
+    if etat not in (2,3,6):
+        return None
+    # print(f"etat = {etat}")
+    return int_value * 10 ** (exp_value * sign_value + float_value)
+
+    # etat = 0
+    # next = peek_char1()
+    # mantisse = 0
+    # flottant_sym = ['e', 'E']
+    # i_flott = -1
+    # pow = 0
+    # sign = 1
+    # while True:
+    #     match etat:
+    #         case 0:
+    #             if next in defs.DIGITS:
+    #                 etat = 3
+    #                 mantisse = mantisse*10 + read_digit()
+    #             elif next == '.':
+    #                 etat = 1
+    #                 consume_char()
+    #             else:
+    #                 return None
+    #         case 1:
+    #             if next in defs.DIGITS:
+    #                 etat = 2
+    #                 mantisse = mantisse + read_digit()*10**i_flott
+    #                 i_flott -= 1
+    #             else:
+    #                 return None
+    #         case 2:
+    #             if next in defs.DIGITS:
+    #                 etat = 2
+    #                 mantisse = mantisse + read_digit()*10**i_flott
+    #                 i_flott -= 1
+    #             elif next in flottant_sym:
+    #                 etat = 4
+    #                 consume_char()
+    #             else:
+    #                 return mantisse
+    #         case 3:
+    #             if next in defs.DIGITS:
+    #                 etat = 3
+    #                 mantisse = mantisse * 10 + read_digit()
+    #             elif next in flottant_sym:
+    #                 etat = 4
+    #                 consume_char()
+    #             elif next == '.':
+    #                 etat = 2
+    #                 consume_char()
+    #             else: 
+    #                 return mantisse
+    #         case 4:
+    #             if next in defs.DIGITS:
+    #                 etat = 6
+    #                 pow = pow * 10 + read_digit()
+    #             elif next in ['+','-']:
+    #                 etat = 5
+    #                 if next == '-':
+    #                     sign = -1
+    #                 consume_char()
+    #             else:
+    #                 return mantisse
+    #         case 5:
+    #             if next in defs.DIGITS:
+    #                 etat = 6
+    #                 pow = pow*10 + read_digit()
+    #             else:
+    #                 return mantisse
+    #         case 6:
+    #             if next in defs.DIGITS:
+    #                 etat = 6
+    #                 pow = pow*10 + read_digit()
+    #             else:
+    #                 return mantisse*10**(pow*sign)
+    #     next = peek_char1()
+    # # return mantisse*10**(pow*sign);
+    # return None
 
 
 # Parse un lexème (sans séparateurs) de l'entrée et renvoie son token.
 # Cela consomme tous les caractères du lexème lu.
 def read_token_after_separators():
     # print("@ATTENTION: lexer.read_token_after_separators à finir !") # LIGNE A SUPPRIMER
-    # n = peek_char1()
-    # tokens = []
-    # while n != defs.EOI:
-    #     if n in defs.DIGITS:
-    #         num = read_NUM()
-    #         if num != None:
-    #             tokens.append((defs.V_T.NUM, num))
-    #     consume_char()
-    #     n = peek_char1()
-    #     print(num)
-    # # return (defs.V_T.END, None) # par défaut, on renvoie la fin de l'entrée
-    # return tokens
     num = read_NUM()
     if num != None:
         return (defs.V_T.NUM, num)
@@ -336,7 +448,8 @@ def read_token_after_separators():
         n = peek_char1()
         consume_char()
         if n == '#':
-            num = read_NUM()
+            num = read_INT()
+            # print(num)
             return (defs.V_T.CALC, num)
         elif n == '+':
              return (defs.V_T.ADD, None)
@@ -399,5 +512,5 @@ def test_lexer():
 if __name__ == "__main__":
     ## Choisir une seule ligne à décommenter
     # test_INT_to_EOI()
-    test_FLOAT_to_EOI()
-    # test_lexer()
+    # test_FLOAT_to_EOI()
+    test_lexer()
